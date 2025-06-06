1970a1966d55bd663b99147ca7481b74e0228c08
Tayen15 <tiantiantitik@gmail.com>
2025-06-06 17:17:42 +0700

    refactor route definitions for consistency and clarity

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,30 @@
 <?php
 
+use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\ParkingController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ParkingController::class, 'index'])->name('welcome');
+Route::get('/', [ParkingController::class, 'index'])->name('index');
 Route::post('/parking', [ParkingController::class, 'store'])->name('parking.store');
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard', [ParkingController::class, 'dashboard'])->name('dashboard');
+    Route::get('/parking', [ParkingController::class, 'dashboard'])->name('dashboard');
+    Route::get('/parking/history', [ParkingController::class, 'history'])->name('parking.history');
     Route::patch('/parking/{id}/exit', [ParkingController::class, 'exit'])->name('parking.exit');
+
+    // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    // Route::put('password', [PasswordController::class, 'update'])->name('password.update');
+    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::get('/login', [ParkingController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [ParkingController::class, 'login'])->name('login');
+Route::middleware('auth')->group(function() {
+    Route::post('/logout', function () {
+        Auth::logout();
+        return redirect()->route('index');
+    })->name('logout');
 });
-
-require __DIR__.'/auth.php';
