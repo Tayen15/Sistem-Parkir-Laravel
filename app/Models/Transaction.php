e0c1f13e7ea8be9af6a73f5507d5fe589f4a6f4b
Tayen15 <tiantiantitik@gmail.com>
2025-06-06 22:09:46 +0700

    fix: correct relationship keys in Transaction model and add duration and fee calculation methods

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,11 +14,40 @@ class Transaction extends Model
 
     public function vehicle()
     {
-        return $this->belongsTo(Vehicle::class, 'id');
+        return $this->belongsTo(Vehicle::class, 'kendaraan_id');
     }
 
-    public function parkirArea()
+    public function areaParkir()
     {
-        return $this->belongsTo(AreaParkir::class, 'id');
+        return $this->belongsTo(AreaParkir::class, 'area_parkir_id');
+    }
+
+    public function calculateDuration($start, $end = null)
+    {
+        $startTime = Carbon::parse($start);
+        $endTime = $end ? Carbon::parse($end) : now();
+
+        $diff = $startTime->diff($endTime);
+        return $diff->h . 'j ' . $diff->i . 'm';
+    }
+
+    public function calculateFee($start, $end = null, VehicleType $vehicleType)
+    {
+        $startTime = Carbon::parse($start);
+        $endTime = $end ? Carbon::parse($end) : now();
+
+        $totalMinutes = $startTime->diffInMinutes($endTime);
+
+        $rate = $vehicleType->fee; 
+
+        $freeDurationThresholdMinutes = 15;
+
+        if ($totalMinutes <= $freeDurationThresholdMinutes) {
+            return 0; 
+        }
+
+        $hours = ceil($totalMinutes / 60);
+
+        return $hours * $rate;
     }
 }
