bd273da88c4f0b89d6ba0b581d48effd1ed9bfd4
Tayen15 <tiantiantitik@gmail.com>
2025-06-07 13:59:13 +0700

    feat: create UserResource for managing user data with forms and tables

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Models\Role;
+use App\Models\User;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Filters\TernaryFilter;
+use Filament\Tables\Filters\TrashedFilter;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-users';
+    protected static ?string $navigationGroup = 'Manajemen Pengguna';
+    protected static ?int $navigationSort = 1;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(256),
+                TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(256)
+                    ->unique(ignoreRecord: true),
+                TextInput::make('password')
+                    ->password()
+                    ->revealable()
+                    ->maxLength(256)
+                    ->dehydrateStateUsing(fn (string $state): string => bcrypt($state))
+                    ->dehydrated(fn (?string $state): bool => filled($state))
+                    ->required(fn (string $operation): bool => $operation === 'create'),
+                Select::make('role')
+                    ->label('Role User')
+                    ->searchable()
+                    ->options(Role::all()->pluck('name', 'id'))
+                    ->required()
+                    ->createOptionForm([
+                        TextInput::make('name')
+                            ->name('Nama Roles Baru')
+                            ->required()
+                            ->maxLength(10)
+                            ->unique(table: Role::class, ignoreRecord: true),
+                    ])
+                    ->createOptionUsing(fn (array $data): int => Role::create($data)->id),
+                Toggle::make('has_changed_password')
+                    ->label('Sudah Ganti Password Awal')
+                    ->default(false),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('email')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('role_rel.name')
+                    ->label('Roles')
+                    ->badge()
+                    ->color(fn (string $state): string => match ($state) {
+                        'User' => 'success',
+                        'Admin' => 'danger',
+                        default => 'gray',
+                    }) 
+                    ->searchable(),
+                IconColumn::make('has_changed_password')
+                    ->label('Pass. Berubah')
+                    ->boolean(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                SelectFilter::make('role')
+                    ->relationship('role_rel', 'name')
+                    ->label('Filter berdasarkan Role'),
+                TernaryFilter::make('has_changed_password')
+                    ->label('Sudah Ganti Password')
+                    ->options([
+                        true => 'Ya',
+                        false => 'Tidak',
+                    ])
+                    ->boolean(),
+                TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\RestoreAction::make(),
+                Tables\Actions\ForceDeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
