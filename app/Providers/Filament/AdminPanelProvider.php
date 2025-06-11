6909d4e5a7bed5f113479426e1d007a19bc8b685
Tayen15 <tiantiantitik@gmail.com>
2025-06-11 08:26:27 +0700

    Add navigation groups to Admin Panel for better organization and structure

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,11 +2,15 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Widgets\DailyTransactionsChart;
+use App\Filament\Widgets\ParkingCapacityAlert;
+use App\Filament\Widgets\PendingTransactionsAlert;
 use App\Filament\Widgets\RecentTransactionsTable;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\NavigationGroup;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
@@ -28,9 +32,24 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->profile()
             ->login()
             ->colors([
                 'primary' => Color::Amber,
+            ])
+            ->navigationGroups([
+                NavigationGroup::make()
+                    ->label('Manajemen Pengguna'), // Nama grup
+
+                NavigationGroup::make()
+                    ->label('Manajemen Parkir'),
+
+                NavigationGroup::make()
+                    ->label('Pengaturan Sistem'),
+
+                NavigationGroup::make()
+                    ->label('Laporan & Statistik'),
+
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
@@ -41,7 +60,10 @@ class AdminPanelProvider extends PanelProvider
             ->widgets([
                 // Widgets\AccountWidget::class,
                 Widgets\StatsOverviewWidget::class,
-                RecentTransactionsTable::class
+                RecentTransactionsTable::class,
+                // DailyTransactionsChart::class,
+                PendingTransactionsAlert::class,
+                ParkingCapacityAlert::class,
             ])
             ->middleware([
                 EncryptCookies::class,
